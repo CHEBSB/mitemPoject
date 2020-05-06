31009c9960c63a264647dd55792edbe989bd1c38
CHEBSB <chebsb229@gmail.com>
2020-05-06 19:47:30 +0800

    moify .py pepae fo new incoming change

--- songList.py
+++ songList.py
@@ -5,21 +5,34 @@ import time
 waitTime = 1.0
 
 # generate the song List
-signal1 = "cxcxgxgxaxaxgyswfxfxexexdxdxcyswswswswsw\n"
-signal2 = "gxexeyswfxdxdyswcxdxexfxgxgxgyswswswswsw\n"
-signal3 = "ewfwgwcxcwcwdwewfxawCwawfwexgwfwdwgwcxsw\n"
+song1 = "cxcxgxgxaxaxgyswfxfxexexdxdxcyswswswswsw\n"
+song2 = "gxexeyswfxdxdyswcxdxexfxgxgxgyswswswswsw\n"
+song3 = "ewfwgwcxcwcwdwewfxawCwawfwexgwfwdwgwcxsw\n"
+song4 = "LwKwLwLwBwLwKxJwEwLwLwEwDwEyEwswswswswsw\n"
+song5 = "awbwCybwCxExbzsxexaygwaxCxgzsxexfxCwbysw\n"
+Songlist = [song1, song2, song3, song4, song5]
 
-# send the waveform table to K66F
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev)
-print("Sending signal ...")
-print("It may take about %d seconds ..." % (int(3 * waitTime)))
+ 
+line = s.readline() # Read an echo string from K66F terminated with '\n'
+# print line
 
-s.write(signal1.encode())
-time.sleep(waitTime)
-s.write(signal2.encode())
-time.sleep(waitTime)
-s.write(signal3.encode())
-time.sleep(waitTime)
-s.close()
-print("Signal sended")
+if line[0] == '9':
+    print("Sending basic songlist ...")
+    print("It may take about %d seconds ..." % (int(len(Songlist) * waitTime)))
+    s.write(Songlist[0].encode())
+    time.sleep(waitTime)
+    s.write(Songlist[1].encode())
+    time.sleep(waitTime)
+    s.write(Songlist[2].encode())
+    time.sleep(waitTime)
+    s.close()
+    print("Signal sended")
+else:
+    print("Sending new song ...")
+    print("It may take about 1 second ..." )
+    s.write(Songlist[int(line[0])].encode())
+    time.sleep(waitTime)
+    s.close()
+    print("New Song arrived!")
