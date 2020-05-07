5a5545ff61aaba52592c2084f3de5459fd4a5dd9
CHEBSB <chebsb229@gmail.com>
2020-05-07 09:37:57 +0800

    a error etection

--- songList.py
+++ songList.py
@@ -16,9 +16,9 @@ serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev)
  
 line = s.readline() # Read an echo string from K66F terminated with '\n'
-# print line
+index = int(line[0])
 
-if line[0] == '9':
+if index == 9:
     print("Sending basic songlist ...")
     print("It may take about %d seconds ..." % (int(len(Songlist) * waitTime)))
     s.write(Songlist[0].encode())
@@ -29,10 +29,14 @@ if line[0] == '9':
     time.sleep(waitTime)
     s.close()
     print("Signal sended")
-else:
+elif index <= 4 and index >= 0:
     print("Sending new song ...")
     print("It may take about 1 second ..." )
-    s.write(Songlist[int(line[0])].encode())
+    s.write(Songlist[index].encode())
     time.sleep(waitTime)
     s.close()
     print("New Song arrived!")
+else:
+    print("Error. Unrecognizable input.")
+    print("Terminate this python program." )
+    s.close()
