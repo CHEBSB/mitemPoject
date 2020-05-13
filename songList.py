482a8dc8d2bf169db46fc7a1a5730cc51bddff34
CHEBSB <chebsb229@gmail.com>
2020-05-13 14:58:00 +0800

    ename vaiable

--- songList.py
+++ songList.py
@@ -12,23 +12,23 @@ Songlist = [song0, song1, song2, song3, song4]
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev)
  
-line = s.read() # Read 
-print("Here PC receive: ", line)
-if line[0] <= 4 and line[0] >= 0:
-    print("Sending new song: Song ", line[0])
+sin = s.read() # Read 
+print("Here PC receive: ", sin)
+if sin <= 4 and sin >= 0:
+    print("Sending new song: Song ", sin)
     print("It may take about 1 second ..." )
-    s.write(Songlist[line[0]].encode())
-    time.sleep(waitTime)
+    s.write(Songlist[sin].encode())
+    time.sleep(1.0)
     s.close()
     print("New Song arrived!")
 else:
     print("Sending basic songlist ...")
     print("It may take about 3 seconds ...")
     s.write(Songlist[0].encode())
-    time.sleep(waitTime)
+    time.sleep(1.0)
     s.write(Songlist[1].encode())
-    time.sleep(waitTime)
+    time.sleep(1.0)
     s.write(Songlist[2].encode())
-    time.sleep(waitTime)
+    time.sleep(1.0)
     s.close()
     print("Signal sended")
